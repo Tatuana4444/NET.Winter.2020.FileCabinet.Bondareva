cb94b27640c1c1f39d24effbcd51bcea0c1fc435
Tatuana4444 <47916561+Tatuana4444@users.noreply.github.com>
2020-03-08 16:56:07 +0300

    Implement FindBy... for FileCabinetFilesystemService

--- FileCabinetApp/FileCabinetFilesystemService.cs
+++ FileCabinetApp/FileCabinetFilesystemService.cs
@@ -12,6 +12,7 @@ namespace FileCabinetApp
     /// </summary>
     public class FileCabinetFilesystemService : IFileCabinetService
     {
+        private readonly CultureInfo englishUS = CultureInfo.CreateSpecificCulture("en-US");
         private FileStream fileStream;
         private IRecordValidator validator;
         private Encoding enc = Encoding.Unicode;
@@ -129,7 +130,29 @@ namespace FileCabinetApp
         /// <returns>Records whith sought-for date of Birth.</returns>
         public ReadOnlyCollection<FileCabinetRecord> FindByDateOfBirth(DateTime dateOfBirth)
         {
-            throw new NotImplementedException();
+            List<FileCabinetRecord> listByDateOfBirth = new List<FileCabinetRecord>();
+            for (int i = 0; i < this.GetStat(); i++)
+            {
+                this.fileStream.Seek((i * 278) + 246, SeekOrigin.Begin);
+                byte[] temp = new byte[12];
+                this.fileStream.Read(temp, 0, 4);
+                int year = BitConverter.ToInt32(temp, 0);
+
+                this.fileStream.Read(temp, 4, 4);
+                int month = BitConverter.ToInt32(temp, 4);
+
+                this.fileStream.Read(temp, 8, 4);
+                int day = BitConverter.ToInt32(temp, 8);
+
+                if (year == dateOfBirth.Year && month == dateOfBirth.Month && day == dateOfBirth.Day)
+                {
+                    this.fileStream.Seek(i * 278, SeekOrigin.Begin);
+                    listByDateOfBirth.Add(this.ReadFromFile());
+                    this.fileStream.Seek(i * 278, SeekOrigin.Begin);
+                }
+            }
+
+            return new ReadOnlyCollection<FileCabinetRecord>(listByDateOfBirth);
         }
 
         /// <summary>
@@ -139,17 +162,36 @@ namespace FileCabinetApp
         /// <returns>Records whith sought-for firstName.</returns>
         public ReadOnlyCollection<FileCabinetRecord> FindByFirstName(string firstName)
         {
-            throw new NotImplementedException();
-        }
+            if (firstName == null)
+            {
+                throw new ArgumentNullException(nameof(firstName), "Firstname can't be null");
+            }
 
-        /// <summary>
-        /// Finds records by last name.
-        /// </summary>
-        /// <param name="lastName">User's last name.</param>
-        /// <returns>Records whith sought-for lastName.</returns>
-        public ReadOnlyCollection<FileCabinetRecord> FindByLastName(string lastName)
-        {
-            throw new NotImplementedException();
+            firstName = firstName.ToUpper(this.englishUS);
+            List<FileCabinetRecord> listByFirstName = new List<FileCabinetRecord>();
+            for (int i = 0; i < this.GetStat(); i++)
+            {
+                this.fileStream.Seek((i * 278) + 6, SeekOrigin.Begin);
+                byte[] tempForStrings = new byte[120];
+                this.fileStream.Read(tempForStrings, 0, 120);
+                int end = 0;
+                while (tempForStrings[end] != '\0' || tempForStrings[end + 1] != '\0')
+                {
+                    end += 2;
+                }
+
+                Array.Resize(ref tempForStrings, end);
+                string currentFirstName = this.enc.GetString(tempForStrings);
+
+                if (currentFirstName.ToUpper(this.englishUS) == firstName)
+                {
+                    this.fileStream.Seek(i * 278, SeekOrigin.Begin);
+                    listByFirstName.Add(this.ReadFromFile());
+                    this.fileStream.Seek(i * 278, SeekOrigin.Begin);
+                }
+            }
+
+            return new ReadOnlyCollection<FileCabinetRecord>(listByFirstName);
         }
 
         /// <summary>
@@ -163,62 +205,7 @@ namespace FileCabinetApp
             this.fileStream.Seek(0, SeekOrigin.Begin);
             for (long i = 0; i < count; i++)
             {
-                byte[] temp = new byte[38];
-                FileCabinetRecord record = new FileCabinetRecord();
-
-                this.fileStream.Read(temp, 0, 2);
-                short status = BitConverter.ToInt16(temp, 0);
-
-                this.fileStream.Read(temp, 2, 4);
-                record.Id = BitConverter.ToInt32(temp, 2);
-
-                byte[] tempForStrings = new byte[120];
-                this.fileStream.Read(tempForStrings, 0, 120);
-                int end = 0;
-                while (tempForStrings[end] != '\0' || tempForStrings[end + 1] != '\0')
-                {
-                    end += 2;
-                }
-
-                Array.Resize(ref tempForStrings, end);
-                record.FirstName = this.enc.GetString(tempForStrings);
-
-                tempForStrings = new byte[120];
-                this.fileStream.Read(tempForStrings, 0, 120);
-                end = 0;
-                while (tempForStrings[end] != '\0' || tempForStrings[end + 1] != '\0')
-                {
-                    end += 2;
-                }
-
-                Array.Resize(ref tempForStrings, end);
-                record.LastName = this.enc.GetString(tempForStrings);
-
-                this.fileStream.Read(temp, 6, 4);
-                int year = BitConverter.ToInt32(temp, 6);
-
-                this.fileStream.Read(temp, 10, 4);
-                int month = BitConverter.ToInt32(temp, 10);
-
-                this.fileStream.Read(temp, 14, 4);
-                int day = BitConverter.ToInt32(temp, 14);
-
-                record.DateOfBirth = new DateTime(year, month, day);
-
-                this.fileStream.Read(temp, 18, 2);
-                record.Gender = BitConverter.ToChar(temp, 18);
-
-                this.fileStream.Read(temp, 20, 2);
-                record.PassportId = BitConverter.ToInt16(temp, 20);
-
-                int[] bytes = new int[4];
-                for (int j = 0; j < 4; j++)
-                {
-                    this.fileStream.Read(temp, 22 + (j * 4), 4);
-                    bytes[j] = BitConverter.ToInt32(temp, 22 + (j * 4));
-                }
-
-                record.Salary = new decimal(bytes);
+                FileCabinetRecord record = this.ReadFromFile();
 
                 list.Add(record);
             }
@@ -233,6 +220,107 @@ namespace FileCabinetApp
         public int GetStat()
         {
             return (int)(this.fileStream.Seek(0, SeekOrigin.End) / 278);
+        }
+
+        /// <summary>
+        /// Finds records by last name.
+        /// </summary>
+        /// <param name="lastName">User's last name.</param>
+        /// <returns>Records whith sought-for lastName.</returns>
+        public ReadOnlyCollection<FileCabinetRecord> FindByLastName(string lastName)
+        {
+            if (lastName == null)
+            {
+                throw new ArgumentNullException(nameof(lastName), "Lastname can't be null");
+            }
+
+            lastName = lastName.ToUpper(this.englishUS);
+            List<FileCabinetRecord> listByLastName = new List<FileCabinetRecord>();
+            for (int i = 0; i < this.GetStat(); i++)
+            {
+                this.fileStream.Seek((i * 278) + 126, SeekOrigin.Begin);
+                byte[] tempForStrings = new byte[120];
+                this.fileStream.Read(tempForStrings, 0, 120);
+                int end = 0;
+                while (tempForStrings[end] != '\0' || tempForStrings[end + 1] != '\0')
+                {
+                    end += 2;
+                }
+
+                Array.Resize(ref tempForStrings, end);
+                string currentFirstName = this.enc.GetString(tempForStrings);
+
+                if (currentFirstName.ToUpper(this.englishUS) == lastName)
+                {
+                    this.fileStream.Seek(i * 278, SeekOrigin.Begin);
+                    listByLastName.Add(this.ReadFromFile());
+                    this.fileStream.Seek(i * 278, SeekOrigin.Begin);
+                }
+            }
+
+            return new ReadOnlyCollection<FileCabinetRecord>(listByLastName);
+        }
+
+        private FileCabinetRecord ReadFromFile()
+        {
+            byte[] temp = new byte[38];
+            FileCabinetRecord record = new FileCabinetRecord();
+
+            this.fileStream.Read(temp, 0, 2);
+            short status = BitConverter.ToInt16(temp, 0);
+
+            this.fileStream.Read(temp, 2, 4);
+            record.Id = BitConverter.ToInt32(temp, 2);
+
+            byte[] tempForStrings = new byte[120];
+            this.fileStream.Read(tempForStrings, 0, 120);
+            int end = 0;
+            while (tempForStrings[end] != '\0' || tempForStrings[end + 1] != '\0')
+            {
+                end += 2;
+            }
+
+            Array.Resize(ref tempForStrings, end);
+            record.FirstName = this.enc.GetString(tempForStrings);
+
+            tempForStrings = new byte[120];
+            this.fileStream.Read(tempForStrings, 0, 120);
+            end = 0;
+            while (tempForStrings[end] != '\0' || tempForStrings[end + 1] != '\0')
+            {
+                end += 2;
+            }
+
+            Array.Resize(ref tempForStrings, end);
+            record.LastName = this.enc.GetString(tempForStrings);
+
+            this.fileStream.Read(temp, 6, 4);
+            int year = BitConverter.ToInt32(temp, 6);
+
+            this.fileStream.Read(temp, 10, 4);
+            int month = BitConverter.ToInt32(temp, 10);
+
+            this.fileStream.Read(temp, 14, 4);
+            int day = BitConverter.ToInt32(temp, 14);
+
+            record.DateOfBirth = new DateTime(year, month, day);
+
+            this.fileStream.Read(temp, 18, 2);
+            record.Gender = BitConverter.ToChar(temp, 18);
+
+            this.fileStream.Read(temp, 20, 2);
+            record.PassportId = BitConverter.ToInt16(temp, 20);
+
+            int[] bytes = new int[4];
+            for (int j = 0; j < 4; j++)
+            {
+                this.fileStream.Read(temp, 22 + (j * 4), 4);
+                bytes[j] = BitConverter.ToInt32(temp, 22 + (j * 4));
+            }
+
+            record.Salary = new decimal(bytes);
+
+            return record;
         }
     }
 }
