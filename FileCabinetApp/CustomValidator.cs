b42d81453d02fdeaf7daf824a8e67b649028518a
Tatuana4444 <47916561+Tatuana4444@users.noreply.github.com>
2020-03-27 00:35:48 +0300

    Using Extract Method for validation.

--- FileCabinetApp/CustomValidator.cs
+++ FileCabinetApp/CustomValidator.cs
@@ -25,6 +25,16 @@ namespace FileCabinetApp
                 throw new ArgumentNullException(nameof(recordData), "RecordData name can't be null");
             }
 
+            this.ValidateFirstName(recordData);
+            this.ValidateLastName(recordData);
+            this.ValidateDateOfBirth(recordData);
+            this.ValidateGender(recordData);
+            this.ValidatePassportId(recordData);
+            this.ValidateSalary(recordData);
+        }
+
+        private void ValidateFirstName(RecordData recordData)
+        {
             if (recordData.FirstName is null)
             {
                 throw new ArgumentNullException(nameof(recordData), "First name can't be null");
@@ -39,7 +49,10 @@ namespace FileCabinetApp
             {
                 throw new ArgumentException("First name can't contain only spaces", nameof(recordData));
             }
+        }
 
+        private void ValidateLastName(RecordData recordData)
+        {
             if (recordData.LastName is null)
             {
                 throw new ArgumentNullException(nameof(recordData), "Last name can't be null");
@@ -54,23 +67,35 @@ namespace FileCabinetApp
             {
                 throw new ArgumentException("Last name can't contain only spaces", nameof(recordData));
             }
+        }
 
+        private void ValidateDateOfBirth(RecordData recordData)
+        {
             if ((DateTime.Compare(new DateTime(1900, 1, 1), recordData.DateOfBirth) > 0)
                 || (DateTime.Compare(DateTime.Now, recordData.DateOfBirth) < 0))
             {
                 throw new ArgumentException("Date of Birth can't be less than 01-Jan-1900 and more than today", nameof(recordData));
             }
+        }
 
+        private void ValidateGender(RecordData recordData)
+        {
             if (recordData.Gender != 'W' && recordData.Gender != 'M')
             {
                 throw new ArgumentException("Gender should be W or M", nameof(recordData));
             }
+        }
 
+        private void ValidatePassportId(RecordData recordData)
+        {
             if (recordData.PassportId < 0)
             {
                 throw new ArgumentException("Passport Id can't be less than 0", nameof(recordData));
             }
+        }
 
+        private void ValidateSalary(RecordData recordData)
+        {
             if (recordData.Salary < 0)
             {
                 throw new ArgumentException($"Salary can't be less than 0", nameof(recordData));
