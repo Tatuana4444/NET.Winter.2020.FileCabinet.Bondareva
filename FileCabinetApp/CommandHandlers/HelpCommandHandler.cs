bbfca17c2238a6f5ae99bbdc7e9507943d1e7ec8
Tatuana4444 <47916561+Tatuana4444@users.noreply.github.com>
2020-05-06 04:34:06 +0300

    Add examples into help.

--- FileCabinetApp/CommandHandlers/HelpCommandHandler.cs
+++ FileCabinetApp/CommandHandlers/HelpCommandHandler.cs
@@ -15,17 +15,17 @@ namespace FileCabinetApp.CommandHandlers
 
         private static readonly string[][] HelpMessages = new string[][]
         {
-            new string[] { "create", "creates new record", "The 'create' command creates new record." },
-            new string[] { "insert", "inserts record", "The 'insert' command inserts record." },
-            new string[] { "update", "updates records by parameters", "The 'update' command updates records by parameters." },
-            new string[] { "delete", "deletes records by parameters", "The 'delete' command deletes records by parameters." },
-            new string[] { "select", "selects records by parameters", "The 'select' command selects records by parameters." },
-            new string[] { "stat", "prints statistics by records", "The 'stat' command prints statistics by records." },
-            new string[] { "export", "exports records", "The 'export' command expords records." },
-            new string[] { "import", "imports records", "The 'import' command imports records." },
-            new string[] { "purge", "purges records", "The 'purge' command purges records deleted records." },
-            new string[] { "help", "prints the help screen", "The 'help' command prints the help screen." },
-            new string[] { "exit", "exits the application", "The 'exit' command exits the application." },
+            new string[] { "create", "creates new record", "The 'create' command creates new record.\nExample: create" },
+            new string[] { "insert", "inserts record", "The 'insert' command inserts record.\nExample: insert (id, firstname, lastname, dateofbirth, gender, salary, passportId) values ('1', 'John', 'Doe', '5/18/1986', 'M', '1234', '1234')" },
+            new string[] { "update", "updates records by parameters", "The 'update' command updates records by parameters.\nExample: update set firstname = 'John', lastname = 'Doe' , dateofbirth = '5/18/1986' where id = '1'" },
+            new string[] { "delete", "deletes records by parameters", "The 'delete' command deletes records by parameters.\nExample: delete where id = '1'" },
+            new string[] { "select", "selects records by parameters", "The 'select' command selects records by parameters.\nExample: select (id, firstname, lastname) where FirstName='Stan' and LastName='Smith'" },
+            new string[] { "stat", "prints statistics by records", "The 'stat' command prints statistics by records.\nExample: stat" },
+            new string[] { "export", "exports records", "The 'export' command expords records.\nExample: export csv fileName.csv" },
+            new string[] { "import", "imports records", "The 'import' command imports records.\nExample: import csv fileName.csv" },
+            new string[] { "purge", "purges records", "The 'purge' command purges records deleted records.\nExample: purge" },
+            new string[] { "help", "prints the help screen", "The 'help' command prints the help screen.\nExample: help" },
+            new string[] { "exit", "exits the application", "The 'exit' command exits the application.\nExample: exit" },
         };
 
         /// <summary>
